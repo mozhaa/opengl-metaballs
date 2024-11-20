42288fe2ff86aec9335122b24a6d6a51b2a15623
mozhaa <mozhay2005@gmail.com>
2024-11-20 18:40:34 +0300

    blank screen

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,180 @@ INITIALIZE_EASYLOGGINGPP
 #include <glm/ext/scalar_constants.hpp>
 #include <glm/gtx/string_cast.hpp>
 
-int main() {
-    return 0;
+#include <stdexcept>
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <cmath>
+
+#include "shaders.h"
+
+
+std::string to_string(std::string_view str) {
+    return std::string(str.begin(), str.end());
+}
+
+void sdl2_fail(std::string_view message) {
+    throw std::runtime_error(to_string(message) + SDL_GetError());
+}
+
+void glew_fail(std::string_view message, GLenum error) {
+    throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
+}
+
+void sdl_init() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        sdl2_fail("SDL_Init: ");
+}
+
+void set_gl_attributes() {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+}
+
+SDL_Window* create_window() {
+    SDL_Window * window = SDL_CreateWindow("Graphics course practice 5",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+
+    if (!window)
+        sdl2_fail("SDL_CreateWindow: ");
+
+    return window;
+}
+
+SDL_GLContext create_context(SDL_Window* window) {
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    if (!gl_context)
+        sdl2_fail("SDL_GL_CreateContext: ");
+    return gl_context;
+}
+
+void glew_init() {
+    if (auto result = glewInit(); result != GLEW_NO_ERROR)
+        glew_fail("glewInit: ", result);
+
+    if (!GLEW_VERSION_3_3)
+        throw std::runtime_error("OpenGL 3.3 is not supported");
+}
+
+GLuint create_shader(GLenum type, const char * source) {
+    GLuint result = glCreateShader(type);
+    glShaderSource(result, 1, &source, nullptr);
+    glCompileShader(result);
+    GLint status;
+    glGetShaderiv(result, GL_COMPILE_STATUS, &status);
+    if (status != GL_TRUE)
+    {
+        GLint info_log_length;
+        glGetShaderiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
+        std::string info_log(info_log_length, '\0');
+        glGetShaderInfoLog(result, info_log.size(), nullptr, info_log.data());
+        throw std::runtime_error("Shader compilation failed: " + info_log);
+    }
+    return result;
+}
+
+GLuint create_program(const std::vector<GLuint>& shaders) {
+    GLuint result = glCreateProgram();
+    for (auto shader: shaders)
+        glAttachShader(result, shader);
+    glLinkProgram(result);
+
+    GLint status;
+    glGetProgramiv(result, GL_LINK_STATUS, &status);
+    if (status != GL_TRUE)
+    {
+        GLint info_log_length;
+        glGetProgramiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
+        std::string info_log(info_log_length, '\0');
+        glGetProgramInfoLog(result, info_log.size(), nullptr, info_log.data());
+        throw std::runtime_error("Program linkage failed: " + info_log);
+    }
+
+    return result;
+}
+
+GLuint create_program_from_sources(const char* vertex_source, const char* fragment_source, const char* geometry_source = "") {
+    std::vector<GLuint> shaders = {};
+    shaders.push_back(create_shader(GL_VERTEX_SHADER, vertex_source));
+    shaders.push_back(create_shader(GL_FRAGMENT_SHADER, fragment_source));
+    if (strlen(geometry_source) > 0)
+        shaders.push_back(create_shader(GL_GEOMETRY_SHADER, geometry_source));
+    auto program = create_program(shaders);
+    return program;
+}
+
+int main(int argc, char* argv[]) try {
+    START_EASYLOGGINGPP(argc, argv);
+    sdl_init();
+    set_gl_attributes();
+    auto window = create_window();
+    auto context = create_context(window);
+    glew_init();
+
+    int width, height;
+    SDL_GetWindowSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    auto program = create_program({
+        create_shader(GL_VERTEX_SHADER, shader_vert),
+        create_shader(GL_FRAGMENT_SHADER, shader_frag),
+    });
+
+    auto last_frame_start = std::chrono::high_resolution_clock::now();
+    float time = 0.f;
+    std::map<SDL_Keycode, bool> button_down;
+
+    glClearColor(0.8f, 0.8f, 1.f, 0.f);
+
+    bool running = true;
+    while (running) {
+        for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
+        {
+        case SDL_QUIT:
+            running = false;
+            break;
+        case SDL_WINDOWEVENT: switch (event.window.event)
+            {
+            case SDL_WINDOWEVENT_RESIZED:
+                width = event.window.data1;
+                height = event.window.data2;
+                glViewport(0, 0, width, height);
+                break;
+            }
+            break;
+        case SDL_KEYDOWN:
+            button_down[event.key.keysym.sym] = true;
+            break;
+        case SDL_KEYUP:
+            button_down[event.key.keysym.sym] = false;
+            break;
+        }
+
+        if (!running)
+            break;
+        
+        auto now = std::chrono::high_resolution_clock::now();
+        float dt = std::chrono::duration_cast<std::chrono::duration<float>>(now - last_frame_start).count();
+        time += dt;
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+} catch (std::exception const & e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
 }
